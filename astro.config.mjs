1c2ada21ce92a9e07b753bf6b605be6e49a59a22
loweffort-alt <farfan_alexander@outlook.com>
2023-09-23 08:57:25 -0500

    New Feature: Deploy ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+  site: "https://loweffort-alt.github.io",
+  base: "/portfolio_v3",
+  integrations: [tailwind(), sitemap()],
 });
