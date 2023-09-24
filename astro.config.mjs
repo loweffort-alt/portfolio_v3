abf4e73ee53a65346d9b1e598b6b5c55253cdb71
loweffort-alt <farfan_alexander@outlook.com>
2023-09-23 21:04:52 -0500

    New Feature: Custom Domain!!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-  site: "https://loweffort-alt.github.io",
-  base: "/portfolio_v3",
+  site: "https://alexfarfan.lat/",
   integrations: [tailwind(), sitemap()],
 });
