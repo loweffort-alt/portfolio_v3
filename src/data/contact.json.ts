5d76ee7d4ca03856cce184c2a7486135f5ec5a75
loweffort-alt <farfan_alexander@outlook.com>
2023-10-18 03:02:09 -0500

    New Feature: Works section

--- src/data/contact.json.ts
+++ src/data/contact.json.ts
@@ -4,14 +4,14 @@ export interface Template {
   title: string;
 }
 const one: Template = {
+  link: "https://loweffort.notion.site/Bienvenido-emprendedor-b6e0e8635bfd412bab98cb731372e3d9",
+  type: "Freelance",
+  title: "¡Dale Vida a tu Presencia en la Web!",
+};
+const two: Template = {
   link: "mailto:farfan_alexander@outlook.com",
   type: "Email",
   title: "farfan_alexander@outlook.com",
-};
-const two: Template = {
-  link: "https://twitter.com/loweffort_alt",
-  type: "Twitter",
-  title: "loweffort_alt",
 };
 const three: Template = {
   link: "https://drive.google.com/file/d/1xTSWrMKQZemCGhtFT4ba5vvw1T90qNZ-/view?usp=sharing",
@@ -19,9 +19,9 @@ const three: Template = {
   title: "Darío Alexander Farfán Navarro",
 };
 const four: Template = {
-  link: "https://loweffort.notion.site/Bienvenido-emprendedor-b6e0e8635bfd412bab98cb731372e3d9",
-  type: "Freelance",
-  title: "¡Dale Vida a tu Presencia en la Web!",
+  link: "https://twitter.com/loweffort_alt",
+  type: "Twitter",
+  title: "loweffort_alt",
 };
 export const bytype = {
   one,
